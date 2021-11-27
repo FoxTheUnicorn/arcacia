9ee6f6969d2e522adb531f392e635116a64822f0
Lucas <lucas.beyel@alumni.fh-aachen.de>
2021-11-27 17:53:01 +0100

    Programmieren der void geheRandom(), public void geheSchritt() und der void setztAufStart() methode in Enemy [von Lucas]

--- src/arcacia/game/objects/enemy/Enemy.java
+++ src/arcacia/game/objects/enemy/Enemy.java
@@ -1,17 +1,21 @@
 package arcacia.game.objects.enemy;
 
+import arcacia.game.handler.LevelHandler;
 import arcacia.game.objects.GameObject;
 import arcacia.game.util.Location;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Enemy extends GameObject {
-	private int = x;
-	private int = y;
-	private int = speed;
-     Location start;
+
+	private int  speed;
+    Location start;
     
     Location spieler;
     
     int Countdown;
+    // maximale anzahl wie oft der zu letzt gesehene punkt des spielers verfolgt wird
+    private static final int maxVerfolgen = 10;// static sagt aus das es nur einmal gemacht wird, wenn die maximale zahl der verfolgung für verschiedene erhöht werden soll muss static entfernt werden
     
 
     /*
@@ -19,11 +23,15 @@ public class Enemy extends GameObject {
      */
 	public Enemy(Location currentLocation){
 		super(currentLocation);
+        start = new Location(currentLocation.getX(),currentLocation.getY());
+        Countdown = -1;
 	}
 
 
 	public Enemy(int x , int y){
 		super(new Location(x,y));
+        start = new Location(x,y);
+        Countdown = -1;
 	}
 
     public boolean bewegeX(int x){ //nimmt 1 oder -1 an, und bewegt in die richtung
@@ -54,31 +62,59 @@ public class Enemy extends GameObject {
 			return true;
 		    }
 	    }
-        //resetttet Countdown
         return false;
     }
+
     void geheRandom(){
-        //aus allen validen richtungen bestimt der gegner eine und geht einen schriutt in diese richtung
+        //aus allen validen Richtungen bestimmt der genera eine und geht einen schritt in diese Richtung
+        int richtung = ThreadLocalRandom.current().nextInt(1,5);//generiert eine zahl aus {1,2,3,4}
+
+        switch (richtung) {
+            case 1:
+                if (bewegeY(1)){ break;}
+            case 2:
+                if (bewegeX(1)){break;}
+            case 3:
+                if (bewegeY(-1)){break;}
+            case 4:
+                if (bewegeX(-1)){break;}
+            //default:
+                //wenn hier hin gelangt ist ein fehler passiert denn dann kann der gegner sich nicht bewegen. eventuell sollte hier ein interrupt geworfen werden
+        }
     }
 
-    void geheSchritt(){ //public? ist die funktion die aufgerufen werden soll um den gegner zu bewegen
+    public void geheSchritt(){ //public? ist die funktion die aufgerufen werden soll um den gegner zu bewegen
         //gegner geht einen schritt
         //bestimmt ob er geziehlt oder randomgeht
         //führt diesen schritt aus
+        if (siehtSpieler()){
+            Countdown = maxVerfolgen;
+        }
+        if (Countdown > 0){
+            bewege_auf_position(spieler);
+        }else {
+            geheRandom();
+        }
     }
 
     void setzeAufStart(){
         //setzt die Position des Gegners auf die StartPosition zurück
-    }
-    
-    public boolean spieler_collision(spieler_location.getLocation());{
-	if (isSame(enemy.getLocation(),spieler.getLocation()))
-	{
-		return 1
-	}
 
-	return 0;
-    
+        currentLocation.setX(start.getX());
+        currentLocation.setY(start.getY());
+        Countdown = -1;
+    }
+
+
+    public boolean spieler_collision(spieler_location.getLocation());
+    {
+        if (isSame(enemy.getLocation(), spieler.getLocation())) {
+            return 1;
+        }
+
+        return 0;
+    }
+
     void bewege_auf_position(Location spieler_location){
     //bewegt sich in Richtung der zuletzt gesehen Position des Spieler
     //wenn der gegner auf der location ist und den spieler nicht mehr sieht soll er nicht hängen bleiben
