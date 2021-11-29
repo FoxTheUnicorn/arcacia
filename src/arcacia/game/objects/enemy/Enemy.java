be0a885e5e769380bc20ab88abb59d3a1bddce51
Lucas <lucas.beyel@alumni.fh-aachen.de>
2021-11-29 17:16:30 +0100

    Überarbeitung aller Funktionen und einfügen von JavaDoc Kommentaren.

--- src/arcacia/game/objects/enemy/Enemy.java
+++ src/arcacia/game/objects/enemy/Enemy.java
@@ -8,18 +8,19 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Enemy extends GameObject {
 
-	private int  speed;
     Location start;
     
     Location spieler;
+    Location last_seen;
     
     int Countdown;
     // maximale anzahl wie oft der zu letzt gesehene punkt des spielers verfolgt wird
     private static final int maxVerfolgen = 10;// static sagt aus das es nur einmal gemacht wird, wenn die maximale zahl der verfolgung für verschiedene erhöht werden soll muss static entfernt werden
-    
+    private static final int sichtWeite = 10;
 
-    /*
-    * @l position an der der Gegner startet und an die der Gegner immer zurückgesetzt wird
+    /**
+     * Konstruktor mit Location Objekt
+     * @param currentLocation übergibt location Object welches die start position des Gegners markiert
      */
 	public Enemy(Location currentLocation){
 		super(currentLocation);
@@ -27,54 +28,117 @@ public class Enemy extends GameObject {
         Countdown = -1;
 	}
 
-
+    /**
+     * Konstruktor mit Location Objekt
+     * @param x Wert der den start X-wert des gegners markiert
+     * @param y Wert der den start Y-wert des gegners markiert
+     */
 	public Enemy(int x , int y){
 		super(new Location(x,y));
         start = new Location(x,y);
         Countdown = -1;
 	}
 
+    /**
+     * Lässt den Gegner einen Schritt in X-Richtung laufen,
+     * sowohl in die positive als auch negative Richtung
+     * @param x 1 lässt in Positiv gehen, -1 in Negativ
+     * @return true wenn erfolgreich, false wenn eine wand im weg ist
+     */
     public boolean bewegeX(int x){ //nimmt 1 oder -1 an, und bewegt in die richtung
         //fragt ob in die gewünschte richtung genug platz zum bewegen ist, wenn ja führe diesen schritt aus, wenn nein geben False zurück
-       int currentX = currentLocation.getX();
-       this.currentLocation.setX(currentX + x);
-       return LevelHandler.isWall(this.currentLocation); //(Location.isSame(this.currentLocation, WallLocation)
-    }
-      public boolean bewegeY(int y){ //nimmt 1 oder -1 an, und bewegt in diese richtung
-        //fragt ob in die gewünschte richtung genug platz zum bewegen ist, wenn ja führe diesen schritt aus, wenn nein geben False zurück
-          //alternative schreibweise, falls eingabe parameter eingeschränkt (nur 1 und -1) muss
-        int currentY = currentLocation.getY();
-        if (y == 1){
-            this.currentLocation.setY(currentY =+ 1);
-            if (!LevelHandler.isWall(this.currentLocation)){return true;}
-            if (LevelHandler.isWall(this.currentLocation)) {
-                this.currentLocation.setY(currentY = -1);
+        if (x == 1 || x == -1){
+            if (LevelHandler.isWall(new Location(this.currentLocation.getX() + x,this.currentLocation.getY()))){
                 return false;
+            }else {
+                this.currentLocation.setX(currentLocation.getX() + x);
+                return true;
             }
         }
-        else if (y ==-1){
-            this.currentLocation.setY(currentY =- 1);
-            if (!LevelHandler.isWall(this.currentLocation)){return true;}
-            if (LevelHandler.isWall(this.currentLocation)) {
-                this.currentLocation.setY(currentY = +1);
-                return false;
-            }
-        }
-        return !LevelHandler.isWall(this.currentLocation); //weiss noch nicht genau was bedingung für true ist
-    }
-
-    public boolean siehtSpieler(){
-	    //playerpos-enemypos == abstand <= 10 blöcke ist soll er ihn verfolgen.
-	    //unter der bedingung das zwischen den beiden positionen keine Wand ist
-	    if((spieler.getlocationX()-enemy.getlocationX())<=10||spieler.getlocationY()-enemy.getlocationY())<=10){
-		    if(!isWall()){
-			target = bewege_auf_position();    
-			return true;
-		    }
-	    }
         return false;
     }
 
+    /**
+     *  Lässt den Gegner einen Schritt in X-Richtung laufen,
+     *      * sowohl in die positive als auch negative Richtung
+     * @param y 1 lässt in Positiv gehen, -1 in Negativ
+     * @return true wenn erfolgreich, false wenn eine wand im weg ist
+     */
+    public boolean bewegeY(int y){ //nimmt 1 oder -1 an, und bewegt in diese richtung
+        // fragt ob in die gewünschte richtung genug platz zum bewegen ist, wenn ja führe diesen schritt aus, wenn nein geben False zurück
+        //alternative schreibweise, falls eingabe parameter eingeschränkt (nur 1 und -1) muss
+
+        if (y == 1 || y == -1){
+            if (LevelHandler.isWall(new Location(this.currentLocation.getX(),this.currentLocation.getY() + y))){
+                return false;
+            }else {
+                this.currentLocation.setY(currentLocation.getY()+y);
+                return true;
+            }
+        }
+          return false;
+    }
+
+    /**
+     * überprüft ob spieler in einer der Richtungen (oben, unten, links, rechts ) ist, wenn eine wand da zwischen ist wird der spieler nicht gesehen
+     * @return true wenn spieler gesehen wurde, false wenn spieler nicht gesehen wurde
+     */
+    public boolean siehtSpieler(){
+	    //playerpos-enemypos == abstand <= 10 blöcke ist soll er ihn verfolgen.
+	    //unter der bedingung das zwischen den beiden positionen keine Wand ist
+        for (int i = 1;i <= sichtWeite; i++)
+        {
+            //x positiv richtung
+            Location cu = new Location(this.currentLocation.getX()+i,this.currentLocation.getY());
+            if (LevelHandler.isWall(cu)) {
+               break;
+            }
+            if (Location.isSame(cu,this.spieler)) {
+            return true;
+            }
+        }
+        for (int i = -1;i >= -sichtWeite; i--)
+        {
+            //x negativ richtung
+            Location cu = new Location(this.currentLocation.getX()+i,this.currentLocation.getY());
+            if (LevelHandler.isWall(cu)) {
+                break;
+            }
+            if (Location.isSame(cu,this.spieler)) {
+                return true;
+            }
+        }
+        for (int i = 1;i <= sichtWeite; i++)
+        {
+            //y positiv richtung
+            Location cu = new Location(this.currentLocation.getX(),this.currentLocation.getY()+i);
+            if (LevelHandler.isWall(cu)) {
+                break;
+            }
+            if (Location.isSame(cu,this.spieler)) {
+                return true;
+            }
+        }
+        for (int i = -1;i >= -sichtWeite; i--)
+        {
+            //y negativ richtung
+            Location cu = new Location(this.currentLocation.getX(),this.currentLocation.getY()+i);
+            if (LevelHandler.isWall(cu)) {
+                break;
+            }
+            if (Location.isSame(cu,this.spieler)) {
+                return true;
+            }
+        }
+
+
+        return false;
+    }
+
+    /**
+     * lässt den gegner in eine der vier richtungen (rechts, oben, links, unten) gehen
+     * entscheidet random in welche
+     */
     void geheRandom(){
         //aus allen validen Richtungen bestimmt der genera eine und geht einen schritt in diese Richtung
         int richtung = ThreadLocalRandom.current().nextInt(1,5);//generiert eine zahl aus {1,2,3,4}
@@ -93,45 +157,59 @@ public class Enemy extends GameObject {
         }
     }
 
-    public void geheSchritt(){ //public? ist die funktion die aufgerufen werden soll um den gegner zu bewegen
+    /**
+     * generelle bewegung von Gegner,
+     * wenn spieler gesehen wurde geht in richtung der zu letzt gesehenen position
+     * sonst gehe einen random schritt
+     * @param s aktuelle Location des Spielers
+     */
+    public void geheSchritt(Location s){ //public? ist die funktion die aufgerufen werden soll um den gegner zu bewegen
         //gegner geht einen schritt
         //bestimmt ob er geziehlt oder randomgeht
         //führt diesen schritt aus
+        spieler = s;
         if (siehtSpieler()){
             Countdown = maxVerfolgen;
+            last_seen = spieler;
         }
         if (Countdown > 0){
-            bewege_auf_position(spieler);
+            bewege_auf_position(last_seen);
+            Countdown--;
         }else {
             geheRandom();
         }
     }
 
+    /**
+     * setzt die position auf die initalen werte zurück,
+     * setzt Countdown auf -1, da der spieler nun nicht mehr gesehen wird
+     */
     void setzeAufStart(){
         //setzt die Position des Gegners auf die StartPosition zurück
 
-        currentLocation.setX(start.getX());
-        currentLocation.setY(start.getY());
+        this.currentLocation.setX(this.start.getX());
+        this.currentLocation.setY(this.start.getY());
         Countdown = -1;
     }
 
 
-    public boolean spieler_collision(spieler_location.getLocation());
-    {
-        if (isSame(enemy.getLocation(), spieler.getLocation())) {
-            return 1;
-        }
+    public boolean spieler_collision() {
+        return (Location.isSame(this.currentLocation, this.spieler)) ;
 
-        return 0;
     }
 
-    void bewege_auf_position(Location spieler_location){
+
+    /**
+     * bewegt sich in die richtung der Location die der funktion übergebenen wird
+     * @param location übergibt ein Location Object, gegner geht in diese richtung wenn möglich
+     */
+    void bewege_auf_position(Location location){
     //bewegt sich in Richtung der zuletzt gesehen Position des Spieler
     //wenn der gegner auf der location ist und den spieler nicht mehr sieht soll er nicht hängen bleiben
-        if (!Location.isSame(this.currentLocation, spieler));
+        if (!Location.isSame(this.currentLocation, spieler))
         {
-            if (this.currentLocation.getX() <= spieler.getX() && this.currentLocation.getY() <= spieler.getY()) {
-                int richtung = ThreadLocalRandom.current().nextInt(1, 2);//generiert eine zahl aus {1,2}
+            if (this.currentLocation.getX() <= location.getX() && this.currentLocation.getY() <= location.getY()) {
+                int richtung = ThreadLocalRandom.current().nextInt(1, 3);//generiert eine zahl aus {1,2}
                 switch (richtung) {
                     case 1:
                         if (bewegeY(1)) {
@@ -144,8 +222,8 @@ public class Enemy extends GameObject {
                         }
                 }
             }
-            if (this.currentLocation.getX() >= spieler.getX() && this.currentLocation.getY() <= spieler.getY()) {
-                int richtung = ThreadLocalRandom.current().nextInt(1, 2);//generiert eine zahl aus {1,2}
+            if (this.currentLocation.getX() >= location.getX() && this.currentLocation.getY() <= location.getY()) {
+                int richtung = ThreadLocalRandom.current().nextInt(1, 3);//generiert eine zahl aus {1,2}
 
                 switch (richtung) {
                     case 1:
@@ -158,8 +236,8 @@ public class Enemy extends GameObject {
                         }
                 }
             }
-            if (this.currentLocation.getX() <= spieler.getX() && this.currentLocation.getY() >= spieler.getY()) {
-                int richtung = ThreadLocalRandom.current().nextInt(1, 2);//generiert eine zahl aus {1,2}
+            if (this.currentLocation.getX() <= location.getX() && this.currentLocation.getY() >= location.getY()) {
+                int richtung = ThreadLocalRandom.current().nextInt(1, 3);//generiert eine zahl aus {1,2}
 
                 switch (richtung) {
                     case 1:
@@ -172,8 +250,8 @@ public class Enemy extends GameObject {
                         }
                 }
             }
-            if (this.currentLocation.getX() >= spieler.getX() && this.currentLocation.getY() >= spieler.getY()) {
-                int richtung = ThreadLocalRandom.current().nextInt(1, 2);//generiert eine zahl aus {1,2}
+            if (this.currentLocation.getX() >= location.getX() && this.currentLocation.getY() >= location.getY()) {
+                int richtung = ThreadLocalRandom.current().nextInt(1, 3);//generiert eine zahl aus {1,2}
 
                 switch (richtung) {
                     case 1:
@@ -187,7 +265,7 @@ public class Enemy extends GameObject {
                 }
             }
         }
-	
-    //Gegner auf dem Bildschirm anzugeben
-	//public void draw();
+
+
+    }
 }
