5740813986ce21249a026604515e5f78210b6040
Lucas <lucas.beyel@alumni.fh-aachen.de>
2021-12-18 19:58:03 +0100

    HighscorePanel nutzt nun die Highscore klasse, um die Top10 gespeicherte Scores auszugeben. Zusätzlich: Funktionen in FileHandler.java sind nun static. Methoden, die die Liste in Highscore zurückgibt. Debug methode in Highscore die 10 DummyWerte in die Liste einträgt. -Lucas Beyel

--- src/arcacia/Arcacia.java
+++ src/arcacia/Arcacia.java
@@ -4,7 +4,6 @@ import arcacia.game.scene.SceneHandler;
 
 public class Arcacia {
     public static void main(String[] args) {
-
         SceneHandler.initialise();
         SceneHandler.showMainMenu();
     }
