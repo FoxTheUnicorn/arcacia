ecc319a63d7a76192f6c822cc73d7442b7f69e93
foxtheunicorn <weedy193@gmail.com>
2021-12-21 16:19:10 +0100

    Changed DoorTile Changed grid width

--- src/arcacia/game/handler/LevelHandler.java
+++ src/arcacia/game/handler/LevelHandler.java
@@ -9,7 +9,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 public class LevelHandler {
-    public final static int level_width = 24; //X
+    public final static int level_width = 25; //X
     public final static int level_height = 15; //Y
     public static int level_number = 1;
     private static GameObject[][] grid = new GameObject[level_width][level_height];
