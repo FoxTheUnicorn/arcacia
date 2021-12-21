c62b44064361e80a3b6e6e4a5682cd77e01b9669
foxtheunicorn <weedy193@gmail.com>
2021-12-21 16:34:07 +0100

    Bug in Level_3.txt

--- src/arcacia/Arcacia.java
+++ src/arcacia/Arcacia.java
@@ -14,6 +14,6 @@ public class Arcacia {
       //  LevelHandler.debugInitGrid(ExtraLife.class);
         SceneHandler.initialise();
         SceneHandler.showMainMenu();
-        FileHandler.loadLevelX(1);
+        FileHandler.loadLevelX(3);
     }
 }
