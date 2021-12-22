c5c7f38cf79159a35e2889ccc1b93c9580f9992f
foxtheunicorn <weedy193@gmail.com>
2021-12-22 18:36:07 +0100

    DEBUG_MDOE can now be activated with program arguments

--- src/arcacia/Arcacia.java
+++ src/arcacia/Arcacia.java
@@ -1,9 +1,18 @@
 package arcacia;
 
+import arcacia.game.handler.ConstantHandler;
 import arcacia.game.scene.SceneHandler;
+
+import java.util.*;
 
 public class Arcacia {
     public static void main(String[] args) {
+        List<String> args2 = Arrays.asList(args);
+        if(args.length != 0) {
+            if(args2.contains("-debug")) {
+                ConstantHandler.DEBUG_MODE = true;
+            }
+        }
         SceneHandler.showMainMenu();
     }
 }
