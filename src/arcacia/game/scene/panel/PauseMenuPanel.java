e59a16c336e2f8d37248446729ee91d2d8ce4b6e
OHeuwes <oscar.heuwes@gmail.com>
2021-12-28 14:12:45 +0100

    Pause Menu komplett

--- src/arcacia/game/scene/panel/PauseMenuPanel.java
+++ src/arcacia/game/scene/panel/PauseMenuPanel.java
@@ -1,5 +1,7 @@
 package arcacia.game.scene.panel;
 
+        import arcacia.game.handler.FileHandler;
+        import arcacia.game.handler.GameHandler;
         import arcacia.game.scene.SceneHandler;
         import arcacia.game.scene.other.MenuButton;
 
@@ -12,10 +14,10 @@ public class PauseMenuPanel extends AbstractMenu {
 
         addSpacer(35);
 
-        add(new MenuButton("Resume", (e) -> {}, Component.CENTER_ALIGNMENT));
+        add(new MenuButton("Resume", (e) -> SceneHandler.showLevel(), Component.CENTER_ALIGNMENT));
         addSpacer(15);
 
-        add(new MenuButton("Restart", (e) -> {}, Component.CENTER_ALIGNMENT));
+        add(new MenuButton("Restart", (e) -> {FileHandler.loadLevelX(GameHandler.getLevel_number()); SceneHandler.showLevel();}, Component.CENTER_ALIGNMENT));
         addSpacer(15);
 
         add(new MenuButton("Tutorial", (e) -> SceneHandler.showTutorial(), Component.CENTER_ALIGNMENT));
